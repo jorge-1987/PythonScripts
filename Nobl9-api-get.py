9688102c4a274c189ca3b184a89b62061e7fd04d
Jorge Abreu <jorge-1987@users.noreply.github.com>
2024-11-12 10:49:20 -0500

    Update and rename Nobl9-api-interaction.py to Nobl9-api-get.py

--- Nobl9-api-get.py
+++ Nobl9-api-get.py
@@ -14,5 +14,16 @@ org = "Org ID"
 headers = {"Accept":"application/json;version=v1alpha","Organization":org}
 
 #Get the token for authentication
-tokenresponse = requests.post('https://app.nobl9.com/api/accessToken',
+tresponse = requests.post('https://app.nobl9.com/api/accessToken',
                               auth=HTTPBasicAuth(cid, cs),headers=headers)
+#Process the json response and save the token
+bearerjson = json.loads(tresponse.content)
+bearertoken = bearerjson["access_token"]
+
+#Add the token to the header
+headers["Authorization"] = "Bearer "+bearertoken
+
+#get the json response payload
+getresponse = requests.get('https://app.nobl9.com/api/v1/slos', headers=headers)
+
+print(getresponse.content)
